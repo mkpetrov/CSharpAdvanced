78f38e516dc6f2494d40ee0995b79e5a02dae3d6
mkpetrov <mkpetrov@abv.bg>
2017-06-07 21:23:34 +0300

    Streams and Files

---
+++ StreamAndFiles/Copy Binary File/CopyBinaryFile.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace Copy_Binary_File
+{
+    class CopyBinaryFile
+    {
+        static void Main(string[] args)
+        {
+            
+            using (FileStream fs=new FileStream("picture.jpg",FileMode.Open))
+            {
+                using (var destination=new FileStream("empty.jpg",FileMode.Create))
+                {
+                    fs.CopyTo(destination);
+                } 
+            }
+        }
+    }
+}
---
+++ StreamAndFiles/Word Count/Word Count.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var output=new Dictionary<string,int>();
+
+            string[] wordsFile = File.ReadAllLines("words.txt");
+            string[] textFile = File.ReadAllLines("text.txt");
+
+            for (int i = 0; i < wordsFile.Length; i++)
+            {
+                foreach (var line in textFile)
+                {
+                    var currentLine = line.Split(new []{' ','-',',','?','!','.'},StringSplitOptions.RemoveEmptyEntries);
+
+                    for (int j = 0; j < currentLine.Length; j++)
+                    {
+                        if (currentLine[j].ToLower()==wordsFile[i])
+                        {
+                            if (!output.ContainsKey(wordsFile[i]))
+                            {
+                                output[wordsFile[i]] = 0;
+                            }
+                            output[wordsFile[i]]++;
+                        }
+                    }
+                }
+            }
+            output = output.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            using (StreamWriter writer=new StreamWriter("results.txt"))
+            {
+                foreach (var word in output)
+                {
+                    var currentWord = word.Key;
+                    var count = word.Value;
+
+                    writer.WriteLine($"{currentWord} - {count}");
+                }
+            }
+        }
+    }
+}
